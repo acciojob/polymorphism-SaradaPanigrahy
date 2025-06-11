fe49a9538fbb7cf2165062dbd52af939d378d738
sarada <sarada.megha.panigrahi@gmail.com>
2025-06-11 23:27:40 +0530

    product_overloading

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p =new Product();
+        System.out.println( p.product(56,32));
+        System.out.println(p.product(23,14,16));
+        System.out.println(p.product(33.56,23.45));
 
+    }
+
+    public static class Product {
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
